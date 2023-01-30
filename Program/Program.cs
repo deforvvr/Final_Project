2c8ee563314dca77adfcabecd3a6a10cd7351159
Egor <eeeee97345@gmail.com>
2023-01-30 14:35:53 +0300

    add some code

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] basedArr = new string[] { "hello", "2", "world", ":-)" };
+            string[] resArr = new string[0];
+
+            foreach (var item in basedArr)
+            {
+                if (item.Length <= 3)
+                {
+                    Array.Resize(ref resArr, resArr.Length + 1);
+                    resArr[resArr.Length - 1] = item;
+                }
+            }
+            Console.WriteLine($"[{String.Join("; ", resArr)}]");
+        }
+    }
